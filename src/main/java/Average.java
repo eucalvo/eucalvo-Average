9ad8843c74daaa34930742c61a5326735a212ae4
eucalvo <eliamcalvo24@gmail.com>
2023-02-13 04:46:07 +0000

    Completed Lab Average

--- src/main/java/Average.java
+++ src/main/java/Average.java
@@ -18,6 +18,10 @@ public class Average {
      * @return the average of all the values of nums.
      */
     public double getAvg(int[] nums){
-        return 0;
+        double average = 0;
+        for (int i = 0; i < nums.length; i++) {
+            average += nums[i];
+        }
+        return average / nums.length;
     }
 }
